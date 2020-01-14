2956fb837e535d9c9083157c142a4612e56e8376
Elizaveta Ivchenko <bettyivchenko@gmail.com>
2020-01-14 17:35:56 +0300

    Add tables creation script

---
+++ table_creations.sql
@@ -0,0 +1,59 @@
+-- create table 'users'
+CREATE TABLE `database`.`users` (
+  `id` INT NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `firstName` VARCHAR(45) NOT NULL,
+  `lastName` VARCHAR(45) NOT NULL,
+  `fatherName` VARCHAR(45) NULL,
+  `phoneNumber` VARCHAR(45) NOT NULL,
+  `isAdmin` TINYINT NOT NULL,
+  PRIMARY KEY (`id`));
+
+--create teble 'catalog'
+CREATE TABLE `database`.`catalog` (
+  `id` INT NOT NULL,
+  `roomType` INT NOT NULL,
+  `capacity` INT NOT NULL,
+  `isTechEquip` TINYINT NOT NULL,
+  `techEquip` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`));
+
+--create table 'booking'
+CREATE TABLE `database`.`booking` (
+  `id` INT NOT NULL,
+  `userId` INT NOT NULL,
+  `toomId` INT NOT NULL,
+  `date` DATE NOT NULL,
+  `startTime` DATETIME NOT NULL,
+  `endTime` DATETIME NOT NULL,
+  `purpose` VARCHAR(200) NULL,
+  `isTechEquip` TINYINT NOT NULL,
+  PRIMARY KEY (`id`));
+
+--create table 'roomtype'
+CREATE TABLE `database`.`roomtype` (
+  `id` INT NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+--create table 'equipment'
+CREATE TABLE `database`.`equipment` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`));
+
+--create table 'equip_for_room'
+CREATE TABLE `database`.`equip_for_room` (
+  `id` INT NOT NULL,
+  `equipId` INT NOT NULL,
+  `roomId` INT NOT NULL,
+  PRIMARY KEY (`id`));
+
+
+--create table 'equip_for_booking'
+CREATE TABLE `database`.`equip_for_book` (
+  `id` INT NOT NULL,
+  `equipId` INT NOT NULL,
+  `bookId` INT NOT NULL,
+  PRIMARY KEY (`id`));
