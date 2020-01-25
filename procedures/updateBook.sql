d6cd246225225a02ac4943071aafb6678b77ebb0
sabdam <52628533+sabdam@users.noreply.github.com>
2020-01-25 13:20:42 +0300

    Update updateBook.sql

--- procedures/updateBook.sql
+++ procedures/updateBook.sql
@@ -1,7 +1,7 @@
 delimiter //
 drop procedure if exists updateBook;
-create procedure updateBook(in id_ int, in userId_ int, in roomId_ int, in date_ DATE, in startTime_ datetime, 
-						in endTime_ datetime, in purpose_ varchar(200), in isTechEquip_ tinyint)
+create procedure updateBook(in id_ int, in userId_ int, in roomId_ int, in date_ DATE, in startTime_ time, 
+						in endTime_ time, in purpose_ varchar(200), in isTechEquip_ tinyint)
 comment "book update"
 begin
 if (userId_ in (select id from `database`.`users`)) and (roomId_ in (select id from `database`.`catalog`))
