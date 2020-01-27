56cc5495ea041fc5e730d68caf39e149069c5e7d
Elizaveta Ivchenko <bettyivchenko@gmail.com>
2020-01-28 02:08:03 +0300

    Add addAnyUsers.* files

---
+++ PolyBook/PolyBook/addAnyUser.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace PolyBook
+{
+    partial class AddAnyUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxCallNum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFatherName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cn = new MySql.Data.MySqlClient.MySqlConnection();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSave.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(147, 327);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(160, 39);
+            this.buttonSave.TabIndex = 27;
+            this.buttonSave.Text = "Добавить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri Light", 12F);
+            this.label7.Location = new System.Drawing.Point(64, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(265, 19);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Введите данные нового пользователя";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(189, 247);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(198, 20);
+            this.textBoxPassword.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri Light", 10F);
+            this.label5.Location = new System.Drawing.Point(65, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Пароль";
+            // 
+            // textBoxCallNum
+            // 
+            this.textBoxCallNum.Location = new System.Drawing.Point(189, 213);
+            this.textBoxCallNum.Name = "textBoxCallNum";
+            this.textBoxCallNum.Size = new System.Drawing.Size(198, 20);
+            this.textBoxCallNum.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri Light", 10F);
+            this.label6.Location = new System.Drawing.Point(65, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "email";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(189, 183);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(198, 20);
+            this.textBoxEmail.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri Light", 10F);
+            this.label3.Location = new System.Drawing.Point(65, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 17);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Номер телефона";
+            // 
+            // textBoxFatherName
+            // 
+            this.textBoxFatherName.Location = new System.Drawing.Point(189, 153);
+            this.textBoxFatherName.Name = "textBoxFatherName";
+            this.textBoxFatherName.Size = new System.Drawing.Size(198, 20);
+            this.textBoxFatherName.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri Light", 10F);
+            this.label4.Location = new System.Drawing.Point(65, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Отчество";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(189, 123);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(198, 20);
+            this.textBoxLastName.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri Light", 10F);
+            this.label2.Location = new System.Drawing.Point(65, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Фамилия";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(189, 93);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(198, 20);
+            this.textBoxName.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri Light", 10F);
+            this.label1.Location = new System.Drawing.Point(65, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Имя";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 29;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(189, 284);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(181, 19);
+            this.checkBox2.TabIndex = 30;
+            this.checkBox2.Text = "Дать права администратора";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // AddAnyUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(450, 393);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxCallNum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxFatherName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "AddAnyUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Создание пользователя";
+            this.Load += new System.EventHandler(this.AddAnyUser_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxCallNum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFatherName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private MySql.Data.MySqlClient.MySqlConnection cn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
---
+++ PolyBook/PolyBook/addAnyUser.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace PolyBook
+{
+    public partial class AddAnyUser : Form
+    {
+        private int Uid;
+        private String strConn;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        public AddAnyUser(int id)
+        {
+            InitializeComponent();
+            Uid = id;
+
+            server = "localhost";
+            database = "database";
+            uid = "root";
+            password = "root";
+            strConn = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            cn.ConnectionString = strConn;
+        }
+
+        private void AddAnyUser_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection failed!!", "Connection", MessageBoxButtons.OK);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            string checkEmail = textBoxEmail.Text;
+            string checkPassword = textBoxPassword.Text;
+            if ((checkEmail.IndexOf("@") != checkEmail.LastIndexOf("@"))
+                || (checkEmail.IndexOf("@") == 0))
+            {
+                MessageBox.Show("Ваш почтовый адрес имеет неверный формат.", "Ошибка регистрации", MessageBoxButtons.OK);
+                return;
+            }
+            if ((checkPassword.IndexOf(" ") >= 0) || (checkPassword == ""))
+            {
+                MessageBox.Show("Ваш пароль имеет неверный формат.", "Ошибка регистрации", MessageBoxButtons.OK);
+                return;
+            }
+            if (!(checkEmail.EndsWith("@edu.spbstu.ru") || checkEmail.EndsWith("@ics2.ecd.spbstu.ru") || checkEmail.EndsWith("@dcn.icc.spbstu.ru")))
+            {
+                MessageBox.Show("Домен вашего почтового адреса не является доменом Университета. Для регистрации укашите почтовый адрес Университета.",
+                    "Ошибка регистрации", MessageBoxButtons.OK);
+                return;
+            }
+
+            MySqlCommand cmd = new MySqlCommand("call addUser(@email, @password, @fn, @ln, @fthn, @pnum, @isAdmin);", cn);
+
+            MySqlParameter email = new MySqlParameter();
+            email = cmd.Parameters.Add("@email", MySqlDbType.VarChar);
+            email.Direction = ParameterDirection.Input;
+            email.Value = textBoxEmail.Text;
+
+            MySqlParameter passw = new MySqlParameter();
+            passw = cmd.Parameters.Add("@password", MySqlDbType.VarChar);
+            passw.Direction = ParameterDirection.Input;
+            passw.Value = textBoxPassword.Text;
+
+            MySqlParameter firstN = new MySqlParameter();
+            firstN = cmd.Parameters.Add("@fn", MySqlDbType.VarChar);
+            firstN.Direction = ParameterDirection.Input;
+            firstN.Value = textBoxName.Text;
+
+            MySqlParameter ln = new MySqlParameter();
+            ln = cmd.Parameters.Add("@ln", MySqlDbType.VarChar);
+            ln.Direction = ParameterDirection.Input;
+            ln.Value = textBoxLastName.Text;
+
+            MySqlParameter fatN = new MySqlParameter();
+            fatN = cmd.Parameters.Add("@fthn", MySqlDbType.VarChar);
+            fatN.Direction = ParameterDirection.Input;
+            fatN.Value = textBoxFatherName.Text;
+
+            MySqlParameter pN = new MySqlParameter();
+            pN = cmd.Parameters.Add("@pnum", MySqlDbType.VarChar);
+            pN.Direction = ParameterDirection.Input;
+            pN.Value = textBoxCallNum.Text;
+
+            int isAdmin = 0;
+
+            if (checkBox2.Checked)
+            {
+                isAdmin = 1;
+            }
+
+            MySqlParameter isAdm = new MySqlParameter();
+            isAdm = cmd.Parameters.Add("@isAdmin", MySqlDbType.VarChar);
+            isAdm.Direction = ParameterDirection.Input;
+            isAdm.Value = isAdmin;
+
+            try
+            {
+                var result = cmd.ExecuteReader();
+                while (result.Read())
+                {
+                    if (Convert.ToInt32(result["id_"].ToString()) == 0)
+                    {
+                        MessageBox.Show("Пользователь с данным почтовым адресом уже существует", "Ошибка в данных", MessageBoxButtons.OK);
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Проверьте правильность введенных данных" + ex, "Ошибка в данных", MessageBoxButtons.OK);
+            }
+
+            AdminPage admPage = new AdminPage(Uid);
+            admPage.tabControl1.SelectedIndex = 1;
+            admPage.Show();
+            this.Close();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
