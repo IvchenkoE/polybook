6554882dd01d6a891f2e531a6543169a2d431d5d
Elizaveta Ivchenko <39265015+IvchenkoE@users.noreply.github.com>
2020-01-28 01:44:45 +0300

    Update addBook.sql

--- procedures/addBook.sql
+++ procedures/addBook.sql
@@ -10,9 +10,9 @@ set id_ = (select max(id) from booking);
 set id_ = id_ +1;
 set c = (select count(id) from booking 
 			where (roomId = roomId_) and (date = date_) and (startTime_ < endTime_) and
-				((startTime_ <= endTime and startTime_ > startTime) or 
-				(endTime_ < endTime and endTime_ >= startTime) or
-                (startTime_ <= startTime and endTime_ >= endTime)));
+				((startTime_ < endTime and startTime_ > startTime) or 
+				(endTime_ < endTime and endTime_ > startTime) or
+                (startTime_ < startTime and endTime_ > endTime)));
 if (c = 0) then insert into `database`.`booking` values(id_ , userId_, roomId_, date_, startTime_, 
 						endTime_, purpose_, isTechEquip_) ;
 end if;
