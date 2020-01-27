a32438b9527b0675e7f69c6376e2c465efde25f5
sabdam <52628533+sabdam@users.noreply.github.com>
2020-01-28 00:58:02 +0300

    Update addBook.sql

--- procedures/addBook.sql
+++ procedures/addBook.sql
@@ -10,9 +10,9 @@ set Id_ = (select max(id) from booking);
 set Id_ = Id_ +1;
 set c = (select count(id) from booking 
 			where (roomId = roomId_) and (date = date_) and (startTime_ < endTime_) and
-				((startTime_ <= endTime and startTime_ > startTime) or 
-				(endTime_ < endTime and endTime_ >= startTime) or
-                (startTime_ <= startTime and endTime_ >= endTime)));
+				((startTime_ < endTime and startTime_ > startTime) or 
+				(endTime_ < endTime and endTime_ > startTime) or
+                (startTime_ < startTime and endTime_ > endTime)));
 if (c = 0) then update `database`.`booking` set userId = userId_, roomId = roomId_, 
 			date = date_, startTime = startTime_, endYime = endTime_, 
             purpose = purpose_, isTechEquip = isTechEquip_ where id = Id_;
