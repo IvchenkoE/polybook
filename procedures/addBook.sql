2ce72184deb3d9e3195030903905472a8ce9297b
sabdam <52628533+sabdam@users.noreply.github.com>
2020-01-27 23:22:13 +0300

    Corrected procedures addBook and updateBook

--- procedures/addBook.sql
+++ procedures/addBook.sql
@@ -1,13 +1,21 @@
 delimiter //
-drop procedure if exists addBook;
-create procedure addBook(in userId_ int, in roomId_ int, in date_ DATE, in startTime_ time, 
+drop procedure if exists newUpdateBook;
+create procedure newUpdateBook(in userId_ int, in roomId_ int, in date_ DATE, in startTime_ time, 
 						in endTime_ time, in purpose_ varchar(200), in isTechEquip_ tinyint)
 comment "adding a new book to the booking"
 begin
-declare maxId int default 0;
-set maxId = (select max(id) from booking)+1;
-if (userId_ in (select id from `database`.`users`)) and (roomId_ in (select id from `database`.`catalog`))
-	then insert into `database`.`booking` values (maxId, userId_, roomId_, date_, startTime_, 
-													endTime_, purpose_, isTechEquip_);
+declare Id_ int default 0;
+declare c int default 0;
+set Id_ = (select max(id) from booking);
+set Id_ = Id_ +1;
+set c = (select count(id) from booking 
+			where (roomId = roomId_) and (date = date_) and (startTime_ < endTime_) and
+				((startTime_ <= endTime and startTime_ > startTime) or 
+				(endTime_ < endTime and endTime_ >= startTime) or
+                (startTime_ <= startTime and endTime_ >= endTime)));
+if (c = 0) then update `database`.`booking` set userId = userId_, roomId = roomId_, 
+			date = date_, startTime = startTime_, endYime = endTime_, 
+            purpose = purpose_, isTechEquip = isTechEquip_ where id = Id_;
 end if;
+select c;
 end//
--- procedures/updateBook.sql
+++ procedures/updateBook.sql
@@ -1,11 +1,18 @@
 delimiter //
-drop procedure if exists updateBook;
-create procedure updateBook(in id_ int, in userId_ int, in roomId_ int, in date_ DATE, in startTime_ time, 
+drop procedure if exists newUpdateBook;
+create procedure newUpdateBook(in id_ int, in userId_ int, in roomId_ int, in date_ DATE, in startTime_ time, 
 						in endTime_ time, in purpose_ varchar(200), in isTechEquip_ tinyint)
-comment "book update"
+comment "adding a new book to the booking"
 begin
-if (userId_ in (select id from `database`.`users`)) and (roomId_ in (select id from `database`.`catalog`))
-	then update `database`.`booking` set userId = userId_, roomId = roomId_, date = date_, 
-    startTime = startTime_, endTime = endTime_, purpose = purpose_, isTechEquip = isTechEquip_ where id = id_;
+declare c int default 0;
+set c = (select count(id) from booking 
+			where (roomId = roomId_) and (date = date_) and (startTime_ < endTime_) and
+				((startTime_ <= endTime and startTime_ > startTime) or 
+				(endTime_ < endTime and endTime_ >= startTime) or
+                (startTime_ <= startTime and endTime_ >= endTime)));
+if (c = 0) then update `database`.`booking` set userId = userId_, roomId = roomId_, 
+			date = date_, startTime = startTime_, endTime = endTime_, 
+            purpose = purpose_, isTechEquip = isTechEquip_ where id = id_;
 end if;
+select c;
 end//
